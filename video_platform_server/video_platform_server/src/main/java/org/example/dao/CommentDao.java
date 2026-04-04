96251d9e021242fdb1c9edb7ba03bf26da164602
Zephyr <fengzi.lv@outlook.com>
2026-04-04 12:09:04 +0800

    feat: 添加CommentDao

--- video_platform_server/video_platform_server/src/main/java/org/example/dao/CommentDao.java
+++ video_platform_server/video_platform_server/src/main/java/org/example/dao/CommentDao.java
@@ -1,5 +1,59 @@
 package org.example.dao;
 
-public class CommentDao {
+import org.example.entity.Comment;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
+
+public class CommentDao extends BaseDao {
+
+    public CommentDao() throws SQLException {
+        super();
+    }
+
+
+    public int addComment(Comment comment) throws Exception {
+        var sql = "INSERT INTO comments (user_id, video_id, context,parent_id) VALUES (?, ?,?, ?)";
+        try (var stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, comment.getUserId());
+            stmt.setInt(2, comment.getVideoId());
+            stmt.setString(3, comment.getContext());
+            stmt.setInt(4, comment.getParentId());
+            return stmt.executeUpdate();
+        }
+    }
+
+    public int updateStatusComment(Comment comment) throws Exception {
+        var sql = "UPDATE comments SET status = ? WHERE id = ?";
+        try (var stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, comment.getStatus());
+            return stmt.executeUpdate();
+        }
+    }
+
+    public List<Comment> getCommentsByVideoId(int videoId) throws Exception {
+        var sql = "SELECT * FROM comments WHERE video_id = ?";
+        try (var stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, videoId);
+            var rs = stmt.executeQuery();
+            var comments = new java.util.ArrayList<Comment>();
+            while (rs.next()) {
+                var comment = new Comment(
+                        rs.getInt("id"),
+                        rs.getInt("user_id"),
+                        rs.getInt("video_id"),
+                        rs.getString("status"),
+                        rs.getTimestamp("create_date"),
+                        rs.getInt("parent_id"),
+                        rs.getString("context")
+                );
+                comments.add(comment);
+            }
+            return comments;
+        }
+    }
+
+
 }
 
--- video_platform_server/video_platform_server/src/main/java/org/example/dao/CommentLikesDao.java
+++ video_platform_server/video_platform_server/src/main/java/org/example/dao/CommentLikesDao.java
@@ -1,5 +1,32 @@
 package org.example.dao;
 
-public class CommentLikesDao {
+import java.sql.SQLException;
+
+public class CommentLikesDao extends BaseDao {
+
+    public CommentLikesDao() throws SQLException {
+        super();
+    }
+
+    public int addLike(int userId, int commentId) throws Exception {
+        var sql = "INSERT INTO comment_likes (user_id, comment_id) VALUES (?, ?)";
+        try (var stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, userId);
+            stmt.setInt(2, commentId);
+            return stmt.executeUpdate();
+        }
+    }
+
+
+    public int deleteLike(int userId, int commentId) throws Exception {
+        var sql = "DELETE FROM comment_likes WHERE user_id = ? AND comment_id = ?";
+        try (var stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, userId);
+            stmt.setInt(2, commentId);
+            return stmt.executeUpdate();
+        }
+    }
+
+
 }
 
